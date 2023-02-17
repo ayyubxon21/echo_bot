2b87bf178254a71dede60477eea3d639c446fa8e
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2023-02-17 10:24:27 -0800

    init

--- client.py
+++ client.py
@@ -2,8 +2,14 @@ from telegram.ext import CallbackContext
 from telegram import Bot, Update
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Welcome to our bot')
+    chat_id = update.message.chat_id
+    bot=context.bot
+
+    bot.send_message(chat_id=chat_id, text='Hello World!')
 
 def echo(update: Update, context: CallbackContext):
-    text=update.message.text
-    update.message.reply_text(text)
+    chat_id = update.message.chat_id
+    text = update.message.text
+    bot=context.bot
+
+    bot.sendMessage(chat_id=chat_id, text=text)
