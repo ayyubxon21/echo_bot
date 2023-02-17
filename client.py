7a868494cd1f5e8d07671d324c6478f3f056604d
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2023-02-17 10:49:12 -0800

    init

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 from telegram.ext import CallbackContext
-from telegram import Bot, Update
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
 
 def start(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
@@ -16,5 +16,19 @@ def echo(update: Update, context: CallbackContext):
 
 def shop(update: Update, context: CallbackContext):
     chat_id=update.message.chat_id
-    bot=context.bot
-    bot.send_message(chat_id=chat_id, text='Welcome')
+    iphone = InlineKeyboardButton(text='Iphone',callback_data='phone_list Apple')
+    samsung = InlineKeyboardButton(text='Samsung',callback_data='phone_list Samsung')
+    huawei = InlineKeyboardButton(text='Huawei',callback_data='phone_list Huawei')
+    xiaomi = InlineKeyboardButton(text='Redmi',callback_data='phone_list Redmi')
+    vivo = InlineKeyboardButton(text='Vivo',callback_data='phone_list Vivo')
+    oppo = InlineKeyboardButton(text='Oppo',callback_data='phone_list Oppo')
+    
+
+    # Define keyboard
+    keyboar = InlineKeyboardMarkup([
+        [iphone,samsung,huawei],
+        [xiaomi,vivo,oppo]
+    ])
+
+    bot = context.bot
+    bot.sendMessage(chat_id=chat_id,text='Choose a phone',reply_markup=keyboar)
